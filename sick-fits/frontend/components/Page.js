da82238264864f732787a3a965035808d3b99c38
Vitnija Bluzma <thepunkyone@gmail.com>
2019-12-18 22:35:40 +0000

    feat: set up global styles and added content to Nav

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -19,9 +19,39 @@ const StyledPage = styled.div`
 
 const Inner = styled.div`
   max-width: ${props => props.theme.maxWidth};
-  background: ${props => props.theme.red};
   margin: 0 auto;
   padding: 2rem;
+`
+
+injectGlobal`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2') format(woff2);
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnika_next';
+  }
+
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
 `
 
 class Page extends Component {
